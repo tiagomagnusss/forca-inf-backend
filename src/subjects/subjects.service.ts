5b75c0eac20ac6f3beb826e954a6653985396675
Lorenzo Lopes Cernicchiaro <lopescernicchiaro@gmail.com>
2023-03-19 18:44:21 -0300

    backend update

--- src/subjects/subjects.service.ts
+++ src/subjects/subjects.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import {
+  Assessment,
+  AssessmentDocument,
+} from 'src/assessments/schemas/assessment.schema';
+import { Teacher, TeacherDocument } from 'src/teachers/schemas/teacher.schema';
 import { CreateSubjectDto } from './dto/create-subject.dto';
 import { UpdateSubjectDto } from './dto/update-subject.dto';
 import { Subject, SubjectDocument } from './schemas/subject.schema';
@@ -10,6 +15,10 @@ export class SubjectsService {
   constructor(
     @InjectModel(Subject.name)
     private readonly subjectModel: Model<SubjectDocument>,
+    @InjectModel(Assessment.name)
+    private readonly assessmentModel: Model<AssessmentDocument>,
+    @InjectModel(Teacher.name)
+    private readonly teacherModel: Model<TeacherDocument>,
   ) {}
 
   async create(createSubjectDto: CreateSubjectDto): Promise<Subject> {
@@ -18,11 +27,57 @@ export class SubjectsService {
   }
 
   async findAll(): Promise<Subject[]> {
-    return this.subjectModel.find().exec();
+    let subjects = await this.subjectModel
+      .find()
+      .sort({ name: 'asc' })
+      .lean()
+      .exec();
+
+    subjects = await Promise.all(
+      subjects.map(async (sub) => {
+        let assessments: any[] = await this.assessmentModel
+          .find({ subject: sub._id })
+          .lean()
+          .exec();
+
+        assessments = await Promise.all(
+          assessments.map(async (a) => {
+            const teacher: Teacher = await this.teacherModel
+              .findById(a.teacher)
+              .lean()
+              .exec();
+
+            return { ...a, teacher };
+          }),
+        );
+
+        return { ...sub, assessments };
+      }),
+    );
+
+    return subjects;
   }
 
   async findOne(id: string): Promise<Subject> {
-    return this.subjectModel.findOne({ _id: id }).lean().exec();
+    const subject = await this.subjectModel.findById(id).lean().exec();
+
+    let assessments: any[] = await this.assessmentModel
+      .find({ subject: id })
+      .lean()
+      .exec();
+
+    assessments = await Promise.all(
+      assessments.map(async (a) => {
+        const teacher: Teacher = await this.teacherModel
+          .findById(a.teacher)
+          .lean()
+          .exec();
+
+        return { ...a, teacher };
+      }),
+    );
+
+    return { ...subject, assessments };
   }
 
   async update(id: string, updateSubjectDto: UpdateSubjectDto) {
